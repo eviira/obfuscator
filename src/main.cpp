beeda2e78b4dbd1e93703c0a2f5367da01aae7d8
eviira <egil.viira@gmail.com>
2025-11-22 17:49:52 +0200

    working implementation for overwriting the file

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,9 @@
 #include "cryptor.cpp"
 
 #pragma section(".replace", read)
-__declspec(allocate(".replace")) INT32 payload_offet_from_me = 2;
-__declspec(allocate(".replace")) UINT32 payload_segment_length = 3;
-__declspec(allocate(".replace")) unsigned payload_key = 1;
+__declspec(allocate(".replace")) INT32 payload_offet_from_me = 1;
+__declspec(allocate(".replace")) UINT32 payload_segment_length = 2;
+__declspec(allocate(".replace")) UINT32 payload_key = 3;
 __declspec(allocate(".replace")) UINT32 replace_file_address = 4;
 __declspec(allocate(".replace")) UINT32 payload_file_address = 5;
 
@@ -43,28 +43,48 @@ int main(int argc, char const *argv[])
 	printf("\n");
 
 	int result = start_payload(argc, argv);
-
-	return result;
-
+	
+	// ----------------------
 	// write new encrypted payload and the corresponding key to the file
-	// TODO
-	// encrypt the payload
+	
+	// encrypt payload with the new key
 	crypt_array(payloadStartAddr, payload_segment_length, next_key, nullptr);
 
-	// update file
-	std::ofstream selfFile(argv[0], std::ios::binary | std::ios::out);
+	// move the running file to a new location
+	// because windows doesnt let you overwrite a running file
+	std::string targetFilePath = std::string(argv[0]);
+	std::string tempFilePath = targetFilePath + ".old";
+	if ( !MoveFileExA(targetFilePath.c_str(), tempFilePath.c_str(), MOVEFILE_REPLACE_EXISTING | MOVEFILE_WRITE_THROUGH) ) {
+		fprintf(stderr, "moving running file: %d", GetLastError());
+		return 1;
+	}
+
+	// create a copy of the file we can edit, put it on the same path the client used to run the program
+	if ( !CopyFileA(tempFilePath.c_str(), targetFilePath.c_str(), true) ) {
+		fprintf(stderr, "copying running file: %d", GetLastError());
+		return 1;
+	}
+
+	// using ofstream or fstream without ios::in causes the file to be truncated
+	std::fstream selfFile(targetFilePath, std::ios::binary | std::ios::out | std::ios::in);
 	if (!selfFile.is_open()) {
 		fprintf(stderr, "couldnt open file\n");
 		return 1;
 	}
 
-	// update payload decryption key
-	selfFile.seekp(replace_file_address, std::ios::beg);
+	// update payload decryption key in file
+	printf("next key: %u\n", next_key);
+	selfFile.seekp(replace_file_address + 8, std::ios::beg);
 	selfFile.write(reinterpret_cast<char*>(&next_key), sizeof next_key);
 
-	// update payload
+	// update payload in file
 	selfFile.seekp(payload_file_address, std::ios::beg);
 	selfFile.write(payloadStartAddr, payload_segment_length);
+
+	// finish
+	selfFile.close();
+
+	// TODO: we are currently leaving tempFilePath around, I could maybe spawn a detached shell to destroy it tho
 
 	return result;
 }

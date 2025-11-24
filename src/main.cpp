f4f9758241d16dddb786c0dbe6e004d4c461fea6
eviira <egil.viira@gmail.com>
2025-11-24 20:21:57 +0200

    restructure main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -14,77 +14,98 @@ __declspec(allocate(".replace")) UINT32 payload_key = 3;
 __declspec(allocate(".replace")) UINT32 replace_file_address = 4;
 __declspec(allocate(".replace")) UINT32 payload_file_address = 5;
 
-int main(int argc, char const *argv[])
-{
-	auto payloadStartAddr = (char*) INT32(&payload_offet_from_me) + payload_offet_from_me;
-	
+char *payload_start_addr;
+const bool print_program_info = false;
+
+void print_program_information() {
 	printf("offset: %i / %x / %p\n", payload_offet_from_me, payload_offet_from_me, &payload_offet_from_me);
 	printf("length: %u / %p\n", payload_segment_length, &payload_segment_length);
 	printf("payload key: %u\n", payload_key);
 	printf("replace file addr: %u\n", replace_file_address);
 	printf("payload file address: %u\n", payload_file_address);
 	printf("payload func addr: %p\n", &start_payload);
-	printf("computed start of payload: %p\n", payloadStartAddr);
-	printf("computed end of payload: %p\n", payloadStartAddr + payload_segment_length);
+	printf("computed start of payload: %p\n", payload_start_addr);
+	printf("computed end of payload: %p\n", payload_start_addr + payload_segment_length);
+}
 
-	printf("payload:\n");
+void print_payload_raw() {
 	for (int i=0; i<payload_segment_length; i++) {
-		printf("%hhx ", *(payloadStartAddr + i));
+		printf("%hhx ", *(payload_start_addr + i));
 	}
 	printf("\n");
+}
 
-	unsigned next_key;
-	crypt_array(payloadStartAddr, payload_segment_length, payload_key, &next_key);
+// write new encrypted payload and the corresponding key to the file
+void overwrite_file(const char *file_path, unsigned next_key) {
+	// create a copy of the memory segment
+	// this is required because we are running the payload After this function
+	char *payload_copy = (char *) malloc(payload_segment_length);
+	std::memcpy(payload_copy, payload_start_addr, payload_segment_length);
 
-	printf("decrypted payload:\n");
-	for (int i=0; i<payload_segment_length; i++) {
-		printf("%hhx ", *(payloadStartAddr + i));
-	}
-	printf("\n");
-
-	int result = start_payload(argc, argv);
-	
-	// ----------------------
-	// write new encrypted payload and the corresponding key to the file
-	
 	// encrypt payload with the new key
-	crypt_array(payloadStartAddr, payload_segment_length, next_key, nullptr);
+	crypt_array(payload_copy, payload_segment_length, next_key, nullptr);
 
 	// move the running file to a new location
 	// because windows doesnt let you overwrite a running file
-	std::string targetFilePath = std::string(argv[0]);
+	std::string targetFilePath = std::string(file_path);
 	std::string tempFilePath = targetFilePath + ".old";
 	if ( !MoveFileExA(targetFilePath.c_str(), tempFilePath.c_str(), MOVEFILE_REPLACE_EXISTING | MOVEFILE_WRITE_THROUGH) ) {
 		fprintf(stderr, "moving running file: %d", GetLastError());
-		return 1;
+		exit(1);
 	}
 
 	// create a copy of the file we can edit, put it on the same path the client used to run the program
 	if ( !CopyFileA(tempFilePath.c_str(), targetFilePath.c_str(), true) ) {
 		fprintf(stderr, "copying running file: %d", GetLastError());
-		return 1;
+		exit(1);
 	}
 
-	// using ofstream or fstream without ios::in causes the file to be truncated
+	// note: using ofstream or fstream without ios::in causes the file to be truncated
 	std::fstream selfFile(targetFilePath, std::ios::binary | std::ios::out | std::ios::in);
 	if (!selfFile.is_open()) {
 		fprintf(stderr, "couldnt open file\n");
-		return 1;
+		exit(1);
 	}
 
-	// update payload decryption key in file
-	printf("next key: %u\n", next_key);
+	// overwrite payload key in file
 	selfFile.seekp(replace_file_address + 8, std::ios::beg);
 	selfFile.write(reinterpret_cast<char*>(&next_key), sizeof next_key);
 
-	// update payload in file
+	// overwrite payload segment in file
 	selfFile.seekp(payload_file_address, std::ios::beg);
-	selfFile.write(payloadStartAddr, payload_segment_length);
+	selfFile.write(payload_copy, payload_segment_length);
 
 	// finish
 	selfFile.close();
+}
 
-	// TODO: we are currently leaving tempFilePath around, I could maybe spawn a detached shell to destroy it tho
+int main(int argc, char const *argv[])
+{
+	// pointer to the start of the payload section in memory
+	// see also: payload_segment_length
+	payload_start_addr = (char*) INT32(&payload_offet_from_me) + payload_offet_from_me;
+	
+	// optional info
+	if (print_program_info) {
+		print_program_information();
+		printf("payload:\n");
+		print_payload_raw();
+	}
 
-	return result;
+	// decrypt the payload section
+	unsigned next_key;
+	crypt_array(payload_start_addr, payload_segment_length, payload_key, &next_key);
+
+	// optional info
+	if (print_program_info) {
+		printf("decrypted payload:\n");
+		print_payload_raw();
+	}
+
+	// write new key + payload to the file
+	overwrite_file(argv[0], next_key);
+	// TODO: we are currently leaving a temporary file around, I could maybe spawn a detached shell to destroy it tho
+	
+	// pass control to payload
+	return start_payload(argc, argv);
 }
